feab677682790d29494fa01d299818433bb8585c
Pranavv-code <pranav.s2080@kws.edu.np>
2024-09-01 14:38:11 +0545

    Add files via upload

---
+++ SignUp.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>
+Sign Up
+
+</title>
+
+</head>
+
+
+<body>
+	<div class=main>
+	Email
+	<input type=email name=em>
+	<br>
+	First Name 
+	<input type=text name=fn>
+	<br>
+	Last Name
+	<input type=text name=ln>
+	<br>
+	Gender 
+	<input type=text name=gen>
+	<br>
+	Password
+	<input type=Password name=pass>
+	<br>
+	<br>
+	<input type=submit value="Sign Up">
+	
+	</div>
+
+
+
+</body>
+
+
+
+</html>
---
+++ log.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <style>
+        .main{
+            display:flex;
+            flex-direction:column;
+            
+        }
+
+        </style>
+</head>
+<body>
+   <div class=main>
+    Email<input type=email name=em>
+    <br>
+
+    Password<input type=email name=pw>
+    <br>
+    <br>
+    <input type=submit value="Log In" name=log>
+</div>
+</body>
+</html>
